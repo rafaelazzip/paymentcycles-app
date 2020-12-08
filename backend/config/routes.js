df1cd0e52eab1385f0376587e96aa2e889d91a8e
Rafael Azzi <rafaelazzipatricio@hotmail.com>
2020-12-08 20:49:22 -0300

    Implementando as Rotas e a API do ciclo de pagamento

---
+++ backend/config/routes.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const billingCycle = require('../api/billingCycle/billingCycle')
+
+module.exports = function(server) {
+
+    // API Routes
+    const router = express.Router()
+    server.use('/api', router)
+
+    // Rotas da API
+    const billingCycleService = require('../api/billingCycle/billingCycleService')
+    billingCycleService.register(router, './billingCycles')
+}
